103efaf3f863ac2a7d60b625b3de9fc540af50ba
CoffeeOwl97 <tcseelig@gmail.com>
2020-04-29 18:48:15 +0100

    cleanup project space and move new code into primary code base

--- src/main/java/com/example/ForumBE/model/Topic.java
+++ src/main/java/com/example/ForumBE/model/Topic.java
@@ -1,4 +1,24 @@
 package com.example.ForumBE.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "forum_topic")
+@Data
+@NoArgsConstructor
 public class Topic {
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Id
+    @NonNull
+    private Long topic_id;
+    @NonNull
+    private String topic_name;
+    @NonNull
+    private String topic_created;
+    @NonNull
+    private String user_id;
 }
